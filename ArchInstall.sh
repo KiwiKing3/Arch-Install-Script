c1d8c1ca5bbbb75b53dd46245c1ad33391ff5fe6
Junis <102294130+KiwiKing3@users.noreply.github.com>
2024-07-08 13:36:42 +0200

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -5,6 +5,7 @@
 
 loadkeys de-latin1
 #partitioning must be added
+cfdisk
 mkfs.fat -F 32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
@@ -19,3 +20,20 @@ pacstrap /mnt base linux linux-firmware sof-firmware base-devel grub efibootmgr 
 genfstab /mnt > /mnt/etc/fstab
 arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
+#
+echo "de_DE.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=de_DE.UTF-8" > /etc/locale.conf
+echo "KEYMAP=de-latin1" > /etc/vconsole.conf
+echo "KiwiArch" > /etc/hostname
+passwd
+useradd -m -G wheel -s /bin/bash IuseArchBTW
+passwd IuseArchBTW
+EDITOR=nano visudo
+systemctl enable NetworkManager
+systemctl enable sddm
+grub-install /dev/sda1
+grub-mkconfig -o /boot /grub/grub.cfg
+exit
+reboot
